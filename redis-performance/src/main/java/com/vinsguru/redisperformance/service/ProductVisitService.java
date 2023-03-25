3b43db45a19f8f47b2348abddbb9534533b4691f
vinsguru <vino.infy@gmail.com>
2023-03-25 08:15:18 -0600

    updated for spring 3.0.X

--- redis-performance/src/main/java/com/vinsguru/redisperformance/service/ProductVisitService.java
+++ redis-performance/src/main/java/com/vinsguru/redisperformance/service/ProductVisitService.java
@@ -11,7 +11,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
